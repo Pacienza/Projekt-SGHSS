1b5f3316731db9a13ec7e31363bef1f49274ca0c
Lucas Pacienza <lucaspacienza55@gmail.com>
2025-04-23 22:51:28 -0300

    fix: ajuste no registro do viewset

--- agenda/urls.py
+++ agenda/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import AgendaProfissionalViewSet
 
 router = DefaultRouter()
-router.register(r'agenda', AgendaProfissionalViewSet)
+router.register(r'agenda', AgendaProfissionalViewSet, basename='agenda')
 
 urlpatterns = router.urls
